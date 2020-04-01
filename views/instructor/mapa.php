1162835f5a0a0384ad8896be3cd7dd8707d734e2
kevinRpo <kevinrestrepo18@gmail.com>
2020-04-01 04:18:37 -0500

    Mapa Incluido

--- views/instructor/mapa.php
+++ views/instructor/mapa.php
@@ -15,30 +15,19 @@
         <link rel="icon" href="../../assets/images/seguiapp.ico" />
         <link rel="stylesheet" href="../../assets/icons/css/all.min.css" />
         <link rel="stylesheet" href="css/estilos.css" />
-        <link rel="stylesheet" href="css/estiloMapa.css" />
-        <script src="https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.js"></script>
-        <link href="https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.css" rel="stylesheet" />
-        <link rel="stylesheet" href="estiloMapa.css">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.2/css/mdb.min.css" rel="stylesheet">
         <title>SeguiApp</title>
     </head>
     <?php require_once './header/header.php'; ?>
     <body>
         <main>
-            <h1>Tu mapa</h1><br/>
-            <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.4.2/mapbox-gl-geocoder.min.js"></script>
-            <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.4.2/mapbox-gl-geocoder.css" type="text/css"/>
-            <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.4.2/mapbox-gl-geocoder.min.js"></script>
-            <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.0.2/mapbox-gl-directions.js"></script> 
-            
-            <link rel="stylesheet"href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.0.2/mapbox-gl-directions.css"type="text/css"/>
-            <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"></script>
-
-            
-            <div id="map"></div>
-            <div id="geocoder" class="geocoder"></div>
-            
-            <script src="js/main.js"></script>
+                <h1>Tu mapa</h1><br/>
+                <section class="container-fluid Contactos p-0 d-flex justify-content-end align-items-center height:10%" id="contactos">
+                <!-- llamamos el mapa desde google maps -->
+                <iframe class="w-100 h-100 d-sm-block position-absolute" 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.2722382127736!2d-75.56388198570384!3d6.2277936282592075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e442847f6fdfe27%3A0xc28cdf908b9199b2!2sCancha%20La%20Esmeralda%20Loreto!5e0!3m2!1ses-419!2sco!4v1585696308381!5m2!1ses-419!2sco"
+                width="1150" height="500" frameborder="0" style="border:0, text-align:center" allowfullscreen></iframe>
+                <div class="view w-100 h-150">
         </main>
     </body>
 </html>
